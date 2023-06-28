3a8d8a87484a8adf2a57dd21045e8820d56eb7c1
nancy <smithnancy@outlook.com>
2023-06-28 11:29:37 +1000

    component header now display the content of the car

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,7 +24,7 @@
                         <img class="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
 
                         <div id="carrito" class="bg-white p-3">
-                            <p v-if="carrito.length === 0" class="text-center">El carrito esta vacio</p>
+                            <p v-if="carrito.length === 0" class="text-center m-0">El carrito esta vacio</p>
                             <div v-else>
                                 <table class="w-100 table">
                                     <thead>
@@ -37,13 +37,16 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr>
+                                        <tr v-for="producto in carrito">
                                             <td>
-                                                <img class="img-fluid" src="/img/guitarra_02.jpg" alt="imagen guitarra">
+                                                <img
+                                                    class="img-fluid"
+                                                    :src="'/img/'+producto.imagen+'.jpg'"
+                                                    :alt="'imagen guitarra '+ producto.imagen">
                                             </td>
-                                            <td>SRV</td>
+                                            <td>{{ producto.nombre }}</td>
                                             <td class="fw-bold">
-                                                    $299
+                                                    $ {{ producto.precio }}
                                             </td>
                                             <td class="flex align-items-start gap-4">
                                                 <button
@@ -52,7 +55,7 @@
                                                 >
                                                     -
                                                 </button>
-                                                    1
+                                                    {{ producto.cantidad }}
                                                 <button
                                                     type="button"
                                                     class="btn btn-dark"
